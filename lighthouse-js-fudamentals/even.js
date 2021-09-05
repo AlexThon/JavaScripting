fe1a50f5251558a3e9af67d1afc9ae49179e00a1
Alex Thon <ghaiquol@gmail.com>
2021-09-05 13:18:58 -0400

    add comments to even.js file

--- lighthouse-js-fudamentals/even.js
+++ lighthouse-js-fudamentals/even.js
@@ -5,5 +5,5 @@ const isEven = function (num) {
 const tenIsEven = isEven(10);
 const elevenIsEven = isEven(11);
 
-console.log(tenIsEven);
-console.log(elevenIsEven);
+console.log(tenIsEven);  // returns true
+console.log(elevenIsEven); // returns false
