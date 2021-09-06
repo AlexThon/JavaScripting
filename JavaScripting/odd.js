25f2f472954dc3906e4ea8c5b6c09da83d2b4b2d
Alex Thon <ghaiquol@gmail.com>
2021-09-06 18:25:29 +0000

    Delete javaScripting folder

--- JavaScripting/odd.js
+++
@@ -1,4 +0,0 @@
-const isOdd = (num) => num % 2 !== 0; 
-
-console.log(isOdd(3)); // expect to return true
-console.log(isOdd(4)) //expected to return false
---
+++ lighthouse-js-fundamentals/intro_to_string.js
@@ -0,0 +1,2 @@
+let stringExample = "String can be enclosed with a single or double quotes like this one here";
+console.log(stringExample);
