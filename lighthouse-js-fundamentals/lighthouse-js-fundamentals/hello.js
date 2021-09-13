075e40575151088d8c3b1c3ae4c85a5f5ccd31d7
Alex Thon <ghaiquol@gmail.com>
2021-09-13 16:32:36 +0000

    delete nested file

--- lighthouse-js-fundamentals/lighthouse-js-fundamentals/hello.js
+++
@@ -1,8 +0,0 @@
-// const sayHello = () => console.log("Hello, world");
-// sayHello();
-
-// improved version
-const sayHello = (name) => console.log("Hello, world " + name);
-sayHello("Donda");
-sayHello("Drake");
-sayHello("Kanye");
