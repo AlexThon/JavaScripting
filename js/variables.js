70fa15c3af1224810d3d4adb6051a9a3bebecf0b
ALEXANDER THON GHAI <89659126+AlexThon@users.noreply.github.com>
2021-09-13 11:52:33 -0400

    Update variables.js

---
+++ js/variables.js
@@ -0,0 +1,15 @@
+var message = "Hello World!";
+console.log(message);
+
+/*
+What is a variable? 
+ - container for the variables
+Variable stores a data value that can be changed later on
+naming convention of variables conform to;
+ - camelCase(myNum) or delimited with under_score for example first_name, first_and_last_name etc.
+ - should not start with a number but may include it in the name e.g take1, take2 
+ 
+
+
+
+*/
